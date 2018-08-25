35773c2349318ef1e1d3dcb483d5e1c62116fe8f
yash101 <lodhad@gmail.com>
2018-08-24 17:53:01 -0700

    Finished build system prototype :). Working on learning AVL trees

---
+++ AVL-Trees/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// A node in an AVL tree
+typedef struct Node {
+	Node* left;
+	Node* right;
+
+	size_t weight;
+} Node;
+
+
+int main() {
+	srand(time(NULL));	// Seed the RNG so we have some randomness to work with
+}
+
+// Recursively delete tree. This could be done iteratively by saving the state
+// while navigating the tree. This is implemented like a DFS
+void delete_tree(Node* tree) {
+	// If the tree has a left node attached
+	if(tree->left) {
+		// Delete the left subtree
+		delete_tree(tree->left);
+	}
+
+	// If the tree has a right subtree attached
+	if(tree->right) {
+		// Delete the right subtree
+		delete_tree(tree->right);
+	}
+
+	// Delete this node
+	free((void*) tree);
+}
